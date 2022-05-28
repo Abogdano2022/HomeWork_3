39e379a7cf819beaf334dee6aceaf111bf4d090c
Anastasia <anastasia.a.bogdanova@gmail.com>
2022-05-28 15:12:42 +0300

    Не сделала 4 задание.

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] apples = new int[3];
+        apples[0] = 1;
+        apples[1] = 2;
+        apples[2] = 3;
+
+        double[] heights = {1.57, 7.654, 9.986};
+
+        int [] x = new int[6];
+        x[0] = 12;
+        x[1] = 15;
+        x[2] = 20;
+        x[3] = 24;
+        x[4] = 30;
+        x[5] = 35;
+
+        System.out.println(apples[0] + "," + apples[1] + "," + apples[2]);
+        System.out.println(heights[0]+ "," + heights[1] + "," + heights[2]);
+        System.out.println(x[0]+ "," +x[1]+ "," +x[2] + "," +x[3]+ "," +x[4]+ "," +x[5]);
+        System.out.println(apples[2] + "," + apples[1] + "," + apples[0]);
+        System.out.println(heights[2]+ "," + heights[1] + "," + heights[0]);
+        System.out.println(x[5]+ "," +x[4]+ "," +x[3] + "," +x[2]+ "," +x[1]+ "," +x[0]);
+
+
+    }
+
+
+    }
